32e4b9a126cbe2720ca11a4c6fab81b226d7191b
young <zerobugpark@gmail.com>
2025-03-20 19:52:42 +0900

    git init

---
+++ Streamify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Streamify
+//
+//  Created by youngkyun park on 3/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
