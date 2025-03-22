38eaff9b813e946447884cf957773fdc93fb41e2
thisisdana49 <thisisdana49@gmail.com>
2025-03-22 14:40:52 +0900

    feat: 네트워크매니저 구성 및 API Call 테스트 진행 #16

--- Streamify/Feature/ViewController.swift
+++ Streamify/Feature/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
 
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        NetworkManager.shared.request(api: .trending, type: TMDBResponse.self)
+            .subscribe(onSuccess: { result in
+                switch result {
+                case .success(let data):
+                    dump("받은 데이터: \(data)")
+                case .failure(let error):
+                    dump("에러 발생: \(error)")
+                }
+            })
+            .disposed(by: disposeBag)
     }
 
 
