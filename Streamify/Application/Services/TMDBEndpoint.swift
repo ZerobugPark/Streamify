0c92d526b08867baf40c5381ba0a64721cbb2a64
thisisdana49 <thisisdana49@gmail.com>
2025-03-22 14:24:16 +0900

    feat: API 요청 시 필요한 EndPoint Enum으로 구성 및 추가 #16

---
+++ Streamify/Application/Services/TMDBEndpoint.swift
@@ -0,0 +1,40 @@
+//
+//  APIKey.swift
+//  TestStreamify
+//
+//  Created by 조다은 on 3/21/25.
+//
+
+import Foundation
+
+enum TMDBEndpoint {
+    static let baseURL = "https://api.themoviedb.org/3"
+
+    case trending
+    case topRated
+    case similar(id: Int)
+    case search
+    case series(id: Int)
+    case season(seriesID: Int, seasonNumber: Int)
+
+    var path: String {
+        switch self {
+        case .trending:
+            return "/trending/tv/day"
+        case .topRated:
+            return "/tv/top_rated"
+        case .similar(let id):
+            return "/tv/\(id)/similar"
+        case .search:
+            return "/search/tv"
+        case .series(let id):
+            return "/tv/\(id)"
+        case .season(let seriesID, let seasonNumber):
+            return "/tv/\(seriesID)/season/\(seasonNumber)"
+        }
+    }
+
+    var fullURL: String {
+        return TMDBEndpoint.baseURL + path
+    }
+}
