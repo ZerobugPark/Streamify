999b3c1e03c88c504b78fe15d90b12fe37ea3164
thisisdana49 <thisisdana49@gmail.com>
2025-03-22 14:44:02 +0900

    chore: 필요 없는 empty 파일 삭제 및 Cell Extension 추가

---
+++ Streamify/Core/Extensions/Reusable.swift
@@ -0,0 +1,20 @@
+//
+//  Reusable.swift
+//  Streamify
+//
+//  Created by 조다은 on 3/22/25.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static var id: String {
+       return String(describing: self)
+    }
+}
+
+extension UICollectionViewCell {
+    static var id: String {
+       return String(describing: self)
+    }
+}
