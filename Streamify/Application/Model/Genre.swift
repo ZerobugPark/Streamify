6fba4eab973b1918832ac6935e1bf24be0e55d68
thisisdana49 <thisisdana49@gmail.com>
2025-03-22 16:24:53 +0900

    feat: Genre 데이터 적절한 한글로 매핑할 수 있도록 Config 수정 및 Genre 모델 추가 #23

---
+++ Streamify/Application/Model/Genre.swift
@@ -0,0 +1,14 @@
+//
+//  Genre.swift
+//  Streamify
+//
+//  Created by 조다은 on 3/22/25.
+//
+
+import Foundation
+
+struct Genre: Codable, Equatable {
+    let id: Int
+    let name: String
+    var isSelected: Bool = false
+}
