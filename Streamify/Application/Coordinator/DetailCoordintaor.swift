1bfbe526c6f11fe778bbe5385beb72a0f44c5282
thisisdana49 <thisisdana49@gmail.com>
2025-03-24 15:54:50 +0900

    feat: 상세화면에서 활용할 DetailCoordinator 생성 및 검색 결과 cell 선택시 상세화면으로 전환되도록 구현 #47

---
+++ Streamify/Application/Coordinator/DetailCoordintaor.swift
@@ -0,0 +1,38 @@
+//
+//  DetailCoordintaor.swift
+//  Streamify
+//
+//  Created by 조다은 on 3/24/25.
+//
+
+import UIKit
+
+final class DetailCoordinator: Coordinator {
+    var navigationController: UINavigationController
+    private let mediaItem: MediaItem
+
+    init(navigationController: UINavigationController, mediaItem: MediaItem) {
+        self.navigationController = navigationController
+        self.mediaItem = mediaItem
+    }
+
+    func start() {
+        showDetailScreen()
+    }
+
+    private func showDetailScreen() {
+        let detailVC = DramaViewController(viewModel: DramaViewModel(dramaID: mediaItem.id))
+        detailVC.coordinator = self
+        navigationController.pushViewController(detailVC, animated: true)
+    }
+
+    func showEpisodeList() {
+        let episodeVC = EpisodeViewController(viewModel: EpisodeViewModel())
+        navigationController.pushViewController(episodeVC, animated: true)
+    }
+
+    func showStarRating() {
+        let starRatingVC = StarRatingStorageViewController(viewModel: StarRatingStorageViewModel())
+        navigationController.pushViewController(starRatingVC, animated: true)
+    }
+}
