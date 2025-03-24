cf5d1c5dfe0e8d3d126dc295d4ebef44123a4fae
young <zerobugpark@gmail.com>
2025-03-24 19:21:34 +0900

    feat: 별점 코멘트 코디네이터 추가

--- Streamify/Feature/StorageBox/StarRatingStorage/View/StarRatingStorageViewController.swift
+++ Streamify/Feature/StorageBox/StarRatingStorage/View/StarRatingStorageViewController.swift
@@ -15,6 +15,7 @@ class StarRatingStorageViewController: BaseViewController<StarRatingStorageView,
     var data: [Rate]
     
     lazy var rateDatas = Observable.just(data)
+    weak var coordinator: DetailCoordinator?
     
     private let filterView = FilterButton()
     
@@ -59,7 +60,12 @@ class StarRatingStorageViewController: BaseViewController<StarRatingStorageView,
             owner.filterView.filterButton.isSelected.toggle()
         }.disposed(by: disposeBag)
     
-        
+        mainView.verticalList.collectionView.rx.modelSelected(Rate.self).bind(with: self) { owner, element in
+            
+            print(element)
+            //coordinator?.showEpisodeList()
+            
+        }.disposed(by: disposeBag)
     }
 
 
