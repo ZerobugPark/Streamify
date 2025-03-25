955a515244a623a979f561d6637c37b58a51c52f
SeBin-Kwon <sbkwon98@gmail.com>
2025-03-25 13:31:28 +0900

    feat: NotificationCenterManager 구현

---
+++ Streamify/Application/Services/NotificationCenterManager.swift
@@ -0,0 +1,34 @@
+//
+//  NotificationCenterManager.swift
+//  Streamify
+//
+//  Created by Sebin Kwon on 3/25/25.
+//
+
+import Foundation
+import RxSwift
+
+protocol NotificationCenterHandler {
+    var name: Notification.Name { get }
+}
+
+extension NotificationCenterHandler {
+    func addObserver() -> Observable<Any?> {
+        return NotificationCenter.default.rx.notification(name).map { $0.object }
+    }
+
+    func post(object: Any? = nil) {
+        NotificationCenter.default.post(name: name, object: object, userInfo: nil)
+    }
+}
+
+enum NotificationCenterManager: NotificationCenterHandler {
+    case progress
+
+    var name: Notification.Name {
+        switch self {
+        case .progress:
+            return Notification.Name("NotificationCenterManager.progress")
+        }
+    }
+}
